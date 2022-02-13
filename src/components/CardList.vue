1812e6a04ed50c560fe198455ba9078fd9c13805
gabrielSzyslak1114 <gr985809@gmail.com>
2022-02-13 11:42:20 +0100

    limpieza de codigo2.0

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -11,22 +11,19 @@
 import {useStore} from 'vuex';
 import {computed, onMounted} from 'vue'
 import Card from './Card.vue';
+
 export default {
   components: { Card },
     setup(){
         const store = useStore()
         const paises = computed(()=> store.getters.topPaisesPoblacion)
+
         onMounted(async() => {
            await store.dispatch('getPaises')
            await store.dispatch('filtrarRegion', 'Americas')
-
         })
            
         return{paises}
     }
 }
 </script>
-
-<style>
-
-</style>
